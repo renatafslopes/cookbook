0d3c4ac5aea968d8713466531d8d14cdefa9f1b2
Renata Lopes <renatafslopes@gmail.com>
2023-02-07 14:25:19 -0300

    inclusão de dados

--- cookbook.rb
+++ cookbook.rb
@@ -1,16 +1,51 @@
-puts "Bem vindo ao Cookbook, sua rede social de receitas"
-
-receitas = [] 
-
-while( 1 == 1 ) do
-puts "Digite o nome da receita: "
-nome = gets.chomp()
-
-receitas << nome
-
-puts
-puts "Receita #{nome} cadastrada com sucesso!"
-puts 
-puts "======== Receitas Cadastradas ========"
-for receita in receitas do
+def bem_vindo()
+    puts "Bem vindo ao Cookbook, sua rede social de receitas"
 end
+
+def menu()
+    puts "[1] Cadastrar uma receita"
+    puts "[2] Ver todas as receitas"
+    puts "[3] Sair"
+
+    print "Escolha uma opção: "
+    return gets.to_i()
+end   
+
+def inserir_receita()
+    puts "Digite o nome da receita: "
+    nome = gets.chomp()
+    puts "Digite o tipo da receita:"
+    tipo = gets.chomp()
+    puts 
+    puts "Receita #{nome} cadastrada com sucesso!"
+    puts
+    return {nome: nome, tipo: tipo }
+end
+
+def imprimir_receitas(receitas)
+    puts "======== Receitas Cadastradas ========"
+    receitas.each do |receita|
+    puts "#{receita[:nome]} - #{receita[:tipo]}"
+     end
+    puts
+end
+
+bem_vindo()
+
+receitas = []
+
+opcao = menu()
+
+while(opcao != 3) do
+  if(opcao == 1)
+    receitas << inserir_receita()
+elsif(opcao == 2)
+    imprimir_receitas(receitas)
+else
+    puts "Opção inválida"
+end
+
+opcao = menu() 
+end
+
+puts 'Obrigado por usar o Cookbook, até logo'
